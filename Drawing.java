a988b7d9a073c463fb079eb07f317a88ed5d5d4c
Jessica Duell <duellrdh@gmail.com>
2018-09-25 21:13:58 -0400

    added rectangle class

--- Drawing.java
+++ Drawing.java
@@ -90,7 +90,48 @@ public class Drawing {
 	}
 	
 	abstract class Shape {
+		Color color;
+		Shape (Color c){
+			color = c;
+		}
+		abstract void firstPoint(Point p);
+		abstract void draw(Graphics g);
+		abstract void subsequentPoint(Point p);
+	}
+	
+	class Rectangle extends Shape {
+		boolean filled = false;
+		Point start;
+		Point lastPoint;
 		
+		Rectangle(Color c, boolean filled){
+			super(c);
+			lastPoint = start;
+			this.filled = filled;
+		}
+		@Override 
+		void firstPoint(Point p) {
+			start = p;
+			lastPoint = p;
+		}
+		@Override
+		void draw (Graphics g) {
+			g.setColor(color);
+			int x = Math.min(start.x, lastPoint.x);
+			int y = Math.min(start.y, lastPoint.y);
+			int w = Math.abs(start.x - lastPoint.x);
+			int h = Math.abs(start.y - lastPoint.y);
+			
+			if (filled) {
+				g.fillRect(x, y, w, h);
+			} else {
+				g.drawRect(x, y, w, h);
+			}
+		}
+		@Override
+		void subsequentPoint(Point p) {
+			lastPoint = p;
+		}
 	}
 	
 }
