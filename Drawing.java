8068d94230655f3d95330fbb06dd9b8296c27f53
Jessica Duell <duellrdh@gmail.com>
2018-09-25 21:08:29 -0400

    updated

--- Drawing.java
+++ Drawing.java
@@ -31,6 +31,66 @@ public class Drawing {
 		return drawingProperties.toString();
 	}
 	
+	public void draw (Graphics g) {
+		for (int i = 0; i < shapeArr.size(); i++) {
+			Shape s = shapeArr.get(i);
+			s.draw(g);
+		}
+		
+		if (inProgress != null) {
+			inProgress.draw(g);
+		}
+	}
 	
+	public void setColor (Color color) {
+		drawingProperties.color = color;
+	}
+	
+	public void setFilled (boolean filled) {
+		drawingProperties.filled = filled;
+	}
+	
+	public void setDrawType (DrawType drawType) {
+		drawingProperties.drawType = drawType;
+	}
+	
+	public void mousePressed(Point p) {
+		switch (drawingProperties.drawType) {
+		case rectangle:
+			inProgress = new Rectangle(drawingProperties.color, drawingProperties.filled);
+			break;
+		case oval:
+			inProgress = new Oval(drawingProperties.color, drawingProperties.filled);
+			break;
+		case line:
+			inProgress = new Line(drawingProperties.color);
+			break;
+		case scribble:
+			inProgress = new Scribble(drawingProperties.color);
+			break;
+		}
+		inProgress.firstPoint(p);
+	}
+	
+	public void mouseDragged(Point p) {
+		switch(drawingProperties.drawType) {
+		case rectangle:
+		case oval:
+		case scribble:
+		case line:
+			inProgress.subsequentPoint(p);
+			break;
+		}
+	}
+	
+	public void mouseReleased(Point p) {
+		inProgress.subsequentPoint(p);
+		shapeArr.add(inProgress);
+		inProgress = null;
+	}
+	
+	abstract class Shape {
+		
+	}
 	
 }
