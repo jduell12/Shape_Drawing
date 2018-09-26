312ea33a4486dc168f339202f6d8e7c4308d66e6
Jessica Duell <duellrdh@gmail.com>
2018-09-25 21:22:13 -0400

    added scribble class

--- Drawing.java
+++ Drawing.java
@@ -134,4 +134,28 @@ public class Drawing {
 		}
 	}
 	
+	class Scribble extends Shape {
+		ArrayList<Point> points = new ArrayList<Point>();
+		
+		Scribble (Color c){
+			super(c);
+		}
+		@Override
+		void firstPoint (Point p) {
+			points.add(p);
+		}
+		@Override
+		void draw (Graphics g) {
+			g.setColor(color);
+			for (int i = 1; i < points.size(); i++) {
+				Point first = points.get(i-1);
+				Point next = points.get(i);
+				g.drawLine(first.x, first.y, next.x, next.y);
+			}
+		}
+		@Override
+		void subsequentPoint (Point p) {
+			points.add(p);
+		}
+	}
 }
